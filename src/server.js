a38d24564ab6cbe2ba9a44faddd6c9d60c9f55c8
Oksana Tanashchuk <oksana.tanashuk@gmail.com>
2025-05-11 16:58:58 +0200

    added crud access

--- src/server.js
+++ src/server.js
@@ -19,6 +19,7 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://localhost:5175',
   'http://localhost:3000',
+  'https://sporthub-1oiu.onrender.com',
 ];
 export const startServer = () => {
   const app = express();
